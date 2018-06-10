9398ca8608d54a24912058734c9fd2beb2f441fa
Andy Clemenko <clemenko@gmail.com>
2018-06-09 21:16:29 -0400

    moar

--- pull_tag_push.sh
+++ pull_tag_push.sh
@@ -16,11 +16,14 @@ docker login -u jenkins -p $DTR_TOKEN $DTR_URL
 docker pull clemenko/dc18:0.1
 docker pull clemenko/dc18:0.2
 docker pull clemenko/dc18:0.3
+docker pull alpine
 #tag
 docker tag clemenko/dc18:0.1 $DTR_URL/ci/dc18_build:0.1
 docker tag clemenko/dc18:0.2 $DTR_URL/ci/dc18_build:0.2
 docker tag clemenko/dc18:0.3 $DTR_URL/ci/dc18_build:0.3
+docker tag alpine $DTR_URL/ci/dc18_build:alpine
 #push
 docker push $DTR_URL/ci/dc18_build:0.1
 docker push $DTR_URL/ci/dc18_build:0.2
 docker push $DTR_URL/ci/dc18_build:0.3
+docker push $DTR_URL/ci/dc18_build:alpine
