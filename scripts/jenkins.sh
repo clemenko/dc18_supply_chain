62e1fc2f29f2b8757e0cd854f6bd95285132cfab
Andy Clemenko <clemenko@gmail.com>
2018-06-10 02:42:16 -0400

    adding jenkins

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -2,16 +2,16 @@
 #this script will create, curl and unpack a client bundle
 
 if [ -z ${DOCS_URL+x} ]; then
-  echo Setting variables for you...
+  echo "Setting variables for you..."
   . ~/dc18_supply_chain/scripts/var_setup.sh
 fi
 
 if [ ! -f ~/dc18_supply_chain/env.sh ]; then
-  echo Creating a client bundle for you...
+  echo "Creating a client bundle for you..."
   . ~/dc18_supply_chain/scripts/client_bundle.sh
 fi
 
 if [ -z ${DOCKER_HOST+x} ]; then
-  echo Sourcing the client bundle for you...
+  echo "Sourcing the client bundle for you..."
   . ~/dc18_supply_chain/env.sh
 fi
